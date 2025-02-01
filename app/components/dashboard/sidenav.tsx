13f245db3a8c9283e10411f00007d5e843605794
Roel-Janssen-44 <roeljanssen2002@gmail.com>
2025-02-01 15:29:25 +0100

    bugfix/removed mobile menu button link

--- app/components/dashboard/sidenav.tsx
+++ app/components/dashboard/sidenav.tsx
@@ -10,8 +10,8 @@ import DashboardMessage from '../dashboardMessage';
 export default async function SideNav() {
   return (
     <div className="z-30 flex h-full flex-col bg-gray-100 p-4 transition-all md:fixed md:left-0 md:top-0 md:h-screen md:min-h-screen md:max-w-[64px] md:p-2 group-hover:md:w-full group-hover:md:max-w-[256px] group-hover:md:shadow-sm">
-      <Link href="/dashboard" className="mb-2 md:mb-4">
-        <div className="flex h-20 items-center justify-start gap-3 rounded-md bg-primary p-3 dark:bg-primary  md:aspect-square md:h-auto md:w-full md:justify-center">
+      <div className="mb-2 flex h-20 items-center justify-start gap-3 rounded-md bg-primary p-3 dark:bg-primary md:mb-4  md:aspect-square md:h-auto md:w-full md:justify-center">
+        <Link href="/dashboard">
           <Image
             src={'/logo_klein_wit.svg'}
             width={56}
@@ -19,12 +19,12 @@ export default async function SideNav() {
             alt="Logo ListTrackr"
             className="w-12"
           />
-          <div className="block flex-1 md:hidden">
-            <DashboardMessage />
-          </div>
-          <MobileNavLinks />
+        </Link>
+        <div className="block flex-1 md:hidden">
+          <DashboardMessage />
         </div>
-      </Link>
+        <MobileNavLinks />
+      </div>
       <div className="hidden grow flex-row justify-between space-x-2 md:flex md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         {/* <SignoutButton /> */}
